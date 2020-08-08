b437564164fd402fe3015b9c524f927bfe72294b
Deepak Kumar Chouhan <dk397787@gmail.com>
2020-08-08 18:40:10 +0530

    Updates

---
+++ precache-manifest.54955b7270592371827702e9491657bd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ba9ca5e1e9470a2a6e1e2ae61e9c67d",
+    "url": "/reactAmazonClone/index.html"
+  },
+  {
+    "revision": "26a5a8cd5f322d08de33",
+    "url": "/reactAmazonClone/static/css/main.6983cc10.chunk.css"
+  },
+  {
+    "revision": "7f7267a442b54443af61",
+    "url": "/reactAmazonClone/static/js/2.c6a354da.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/reactAmazonClone/static/js/2.c6a354da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26a5a8cd5f322d08de33",
+    "url": "/reactAmazonClone/static/js/main.46fa7dad.chunk.js"
+  },
+  {
+    "revision": "7cc2af356daafb680f62",
+    "url": "/reactAmazonClone/static/js/runtime-main.12d2a51e.js"
+  },
+  {
+    "revision": "d86ccbaf5a2e2087ef0316bd39912365",
+    "url": "/reactAmazonClone/static/media/amazon.d86ccbaf.png"
+  },
+  {
+    "revision": "0bbad6c3d79d4588a37b35de6a64cc89",
+    "url": "/reactAmazonClone/static/media/prime.0bbad6c3.png"
+  },
+  {
+    "revision": "4ec08347bb730c13c99d7eff5865a055",
+    "url": "/reactAmazonClone/static/media/primeIndependence.4ec08347.jpg"
+  }
+]);
